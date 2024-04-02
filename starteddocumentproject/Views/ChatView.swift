9178d0cb4a38288f07eacf9bd24f61026d3d61b8
Daniel Caminero <dcaminero@serbatic.es>
2024-04-02 11:54:50 +0200

    chat layout and logic added

--- starteddocumentproject/Views/ChatView.swift
+++ starteddocumentproject/Views/ChatView.swift
@@ -8,11 +8,58 @@
 import SwiftUI
 
 struct ChatView: View {
+    
+    @State var chatMessages: [ChatMessage] = ChatMessage.sampleMessages
+    @State var messageText: String = ""
+    
     var body: some View {
         VStack {
-            Text("This is the Second View")
+            ScrollView{
+                LazyVStack {
+                    ForEach(chatMessages, id: \.id){ message in
+                        messageView(message: message)
+                    }
+                }
+                .navigationTitle("Second View")
+            }
+            HStack{
+                TextField("Enter a message", text: $messageText){
+                    
+                }
+                    .background(.gray.opacity(0.1))
+                    .padding()
+                    .cornerRadius(12)
+                Button {
+                    sendMessage()
+                } label: {
+                    Text("Send")
+                        .foregroundColor(.white)
+                        .background(.black)
+                        .padding()
+                        .cornerRadius(12)
+                }
+
+            }
         }
-        .navigationTitle("Second View")
+        .padding()
+    }
+    
+    func messageView(message: ChatMessage) -> some View {
+        HStack{
+            if message.sender == .me { Spacer() }
+            Text(message.content)
+                .foregroundColor(message.sender == .me ? .white : .black)
+                .padding()
+                .background(message.sender == .me ? .blue : .gray.opacity(0.1))
+                .cornerRadius(16)
+                
+            if message.sender == .gpt { Spacer() }
+        }
+    }
+    
+    func sendMessage() {
+        messageText = " "
+        print(messageText)
     }
 }
 
@@ -20,4 +67,25 @@ struct ChatView_Previews: PreviewProvider {
     static var previews: some View {
         ChatView()
     }
+}
+
+struct ChatMessage {
+    let id: String
+    let content: String
+    let dateCreated: Date
+    let sender: MessageSender
+}
+
+enum MessageSender {
+    case me
+    case gpt
+}
+
+extension ChatMessage {
+    static let sampleMessages = [
+        ChatMessage(id: UUID().uuidString, content: "Sample message from me", dateCreated: Date(), sender: .me),
+        ChatMessage(id: UUID().uuidString, content: "Sample message from gpt", dateCreated: Date(), sender: .gpt),
+        ChatMessage(id: UUID().uuidString, content: "Sample message from me", dateCreated: Date(), sender: .me),
+        ChatMessage(id: UUID().uuidString, content: "Sample message from gpt", dateCreated: Date(), sender: .gpt)
+    ]
 }
